9813a35c61a48b11d32985b809f5046046d7e9c8
Schnizer, Pierre <pierre.schnizer@helmholtz-berlin.de>
2020-05-18 10:16:39 +0200

    flake 8 compatible

--- naus/xmlrpc_server.py
+++ naus/xmlrpc_server.py
@@ -5,11 +5,25 @@ import functools
 
 logger = logging.getLogger('naus')
 
+
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
+
 def setup_xml_server(environment, log=None, server_log_requests=False):
+    '''Returns a closure for setting up the xmlrpc server
+
+    Args:
+       environment:         an instance of
+                            :class:`naus.environment.Environment`
+       log:                 a :class:`logging.Logger` instance
+       server_log_requests: shall server requests be logged
+
+    Returns:
+       a :any:`functools.partial` lambda function for starting and
+       running the server.
+    '''
     if log is None:
         log = logger
 
@@ -23,13 +37,15 @@ def setup_xml_server(environment, log=None, server_log_requests=False):
             Separate implementation in an independent module
         '''
 
-        with SimpleXMLRPCServer(('127.0.0.1', 8000), logRequests=server_log_requests, allow_none=True,
-                                #requestHandler=RequestHandler
+        with SimpleXMLRPCServer(('127.0.0.1', 8000),
+                                logRequests=server_log_requests,
+                                allow_none=True,
+                                # requestHandler=RequestHandler
                                 ) as server:
             server.register_introspection_functions()
             server.register_instance(proxy)
             server.allow_none = True
-    
+
             log.info(f'XML Server running env = {proxy.__class__.__name__}')
             # Run the server's main loop
             server.serve_forever()
